0d9fa77840949b5a52e88f7f786fe46d2fd90223
Elmo Todurov <elmo.todurov@eesti.ee>
2017-11-22 22:42:12 +0000

    Numpad automatically opens doors now

--- macros.js
+++ macros.js
@@ -1,39 +1,81 @@
 var Macros = function(send) {
     var exports = {};
 
-    var macros = {
-         96: function() {send("numpad0")},
-         97: function() {send("sw")},
-         98: function() {send("s")},
-         99: function() {send("se")},
-        100: function() {send("w")},
-        101: function() {send("numpad5")},
-        102: function() {send("e")},
-        103: function() {send("nw")},
-        104: function() {send("n")},
-        105: function() {send("ne")},
-        106: function() {send("numpadMultiply")},
-        107: function() {send("numpadAdd")},
-        109: function() {send("numpadSubtract")},
-        110: function() {send("numpadDecimal")},
-        111: function() {send("numpadDivide")},
-        112: function() {send("macroF1")},
-        113: function() {send("macroF2")},
-        114: function() {send("macroF3")},
-        115: function() {send("macroF4")},
-        116: function() {send("macroF5")},
-        117: function() {send("macroF6")},
-        118: function() {send("macroF7")},
-        119: function() {send("macroF8")},
-        120: function() {send("macroF9")},
-        121: function() {send("macroF10")},
-        122: function() {send("macroF11")},
-        123: function() {send("macroF12")}
+    var macros_backup = {
+         96: "numpad0",
+         97: "sw",
+         98: "s",
+         99: "se",
+        100: "w",
+        101: "numpad5",
+        102: "e",
+        103: "nw",
+        104: "n",
+        105: "ne",
+        106: "numpadMultiply",
+        107: "d",
+        109: "u",
+        110: "numpadDecimal",
+        111: "numpadDivide",
+        112: "macroF1",
+        113: "macroF2",
+        114: "macroF3",
+        115: "macroF4",
+        116: "macroF5",
+        117: "macroF6",
+        118: "macroF7",
+        119: "macroF8",
+        120: "macroF9",
+        121: "macroF10",
+        122: "macroF11",
+        123: "macroF12"
     }
+    var macros = {};
+
+    var reverse_mapping = {
+        'sw': 97,
+        's': 98,
+        'se': 99,
+        'w': 100,
+        'e': 102,
+        'nw': 103,
+        'n': 104,
+        'ne': 105,
+        'd': 107,
+        'u': 109
+    }
+
+    for (var k in macros_backup)
+        macros[k] = macros_backup[k];
+
+    gmcp.handle("room.info", function(ri) {
+        var present = {
+            'nw': false,
+            'ne': false,
+            'se': false,
+            'sw': false,
+            'n': false,
+            'e': false,
+            's': false,
+            'w': false,
+            'u': false,
+            'd': false
+        }
+
+        for (var e in ri['exit_kw'])
+            present[e] = true;
+
+        for (var e in present) {
+            if (present[e])
+                macros[reverse_mapping[e]] = 'open ' + ri['exit_kw'][e] + ' ' + e + ';' + e;
+            else
+                macros[reverse_mapping[e]] = macros_backup[reverse_mapping[e]];
+        }
+    });
 
     exports.run = function(keyCode) {
         if (keyCode in macros) {
-            macros[keyCode]();
+            send(macros[keyCode]);
             return true;
         }
         return false;
